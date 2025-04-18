ea9fbd0fe518c917d79f09215a69402e27f28ea4
zlzonkh <kinggodkhlive@gmail.com>
2025-04-18 13:57:27 +0900

    boj/2743: 단어 길이 재기

---
+++ 새싹/07_문자열/2743_단어_길이_재기.js
@@ -0,0 +1,7 @@
+const problemNum = 2743;
+const input = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./testcase/" + problemNum + ".txt")
+    .toString()
+    .trim()
+    
+console.log(input.length);
