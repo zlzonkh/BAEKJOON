ba807a6cb75b5b2f1bdfce7f3b42d64caabad70a
zlzonkh <kinggodkhlive@gmail.com>
2025-04-20 17:17:16 +0900

    boj/11718: 그대로 출력하기

---
+++ 새싹/07_문자열/11718_그대로_출력하기.js
@@ -0,0 +1,7 @@
+const problemNum = 11718;
+const input = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./testcase/" + problemNum + ".txt")
+    .toString()
+    .trim();
+
+console.log(input);
