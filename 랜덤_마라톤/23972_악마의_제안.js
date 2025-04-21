36021cff741005c900a44fc96f629538e15a2895
zlzonkh1 <zlzonkh1@gmail.com>
2025-04-21 23:50:06 +0900

    boj/23972: 악마의 제안

---
+++ 랜덤_마라톤/23972_악마의_제안.js
@@ -0,0 +1,15 @@
+const problemNum = 23972;
+const [K, N] = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./testcase/" + problemNum + ".txt")
+    .toString()
+    .trim()
+    .split(' ')
+    .map(Number);
+let output = 0;
+
+if(N == 1) console.log(-1);
+else {
+  output = Math.ceil((K * N) / (N - 1));
+  if((output - K) * N < output) output += 1;
+  console.log(output);
+};
