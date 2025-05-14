2589ab095b978880c0af7bdcf8162bc59640f4f1
zlzonkh <kinggodkhlive@gmail.com>
2025-05-14 10:57:57 +0900

    boj/2579: 계단 오르기

--- CLASS_3/2579_계단_오르기.js
+++ CLASS_3/2579_계단_오르기.js
@@ -14,30 +14,24 @@ const N = input[0];
 const stairs = input.slice(1);
 stairs.unshift(0);
 
-const dp = [0, stairs[1]];
-const canStapNext = [true, true];
+const dp = [0, stairs[1], stairs[1] + stairs[2]];
+const canStapNext = [true, true, false];
 
-for (let i = 2; i <= N; i++) {
-  if (canStapNext[i - 1]) {
-    dp.push(dp[i - 1] + stairs[i]);
-    canStapNext.push(false);
+for (let i = 3; i <= N; i++) {
+  const candidate = [];
+  if (i >= 4) candidate.push(dp[i - 4] + stairs[i - 2]);
+  if (canStapNext[i - 3]) candidate.push(dp[i - 3] + stairs[i - 2]);
+  else if (i >= 5) candidate.push(dp[i - 5] + stairs[i - 3] + stairs[i - 2]);
+
+  if (
+    candidate.length >= 1 &&
+    Math.max(...candidate) >= dp[i - 3] + stairs[i - 1]
+  ) {
+    dp.push(Math.max(...candidate) + stairs[i]);
+    canStapNext.push(true);
   } else {
-    const candidate = [];
-    if (i >= 4) candidate.push(dp[i - 4] + stairs[i - 2]);
-    if (canStapNext[i - 3]) candidate.push(dp[i - 3] + stairs[i - 2]);
-
-    if (
-      candidate.length >= 1 &&
-      Math.max(...candidate) >= dp[i - 3] + stairs[i - 1]
-    ) {
-      dp.push(Math.max(...candidate) + stairs[i]);
-      canStapNext.push(true);
-    } else {
-      dp.push(dp[i - 3] + stairs[i - 1] + stairs[i]);
-      canStapNext.push(false);
-    }
+    dp.push(dp[i - 3] + stairs[i - 1] + stairs[i]);
+    canStapNext.push(false);
   }
 }
-console.log(stairs);
-console.log(dp);
 console.log(dp[N]);
