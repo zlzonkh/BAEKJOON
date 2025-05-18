75f6d04d202d92fbf68e58596e993cbcf25ea4a4
zlzonkh1 <zlzonkh1@gmail.com>
2025-05-18 13:24:55 +0900

    boj/16430: 제리와 톰

---
+++ 랜덤_마라톤/코스50/16430_제리와_톰.js
@@ -0,0 +1,13 @@
+const problemNum = 16430;
+const [A, B] = require("fs")
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : "./testcase/" + problemNum + ".txt"
+  )
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(B - A, B);
