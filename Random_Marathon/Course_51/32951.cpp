64d93411aacd706689f1c2b7d34993ee847025a5
zlzonkh <kinggodkhlive@gmail.com>
2025-05-21 09:26:03 +0900

    boj/32951: AI 선도대학

---
+++ Random_Marathon/Course_51/32951.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("./testcase/32951.txt", "r", stdin);
+    #endif
+
+    int N;
+    cin >> N;
+    cout << N - 2024 << '\n';
+}
