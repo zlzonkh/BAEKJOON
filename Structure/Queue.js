d71003611c1d0c378aadac414dd87ca26806f3aa
zlzonkh <kinggodkhlive@gmail.com>
2025-05-28 11:05:48 +0900

    util: Queue.js 추가

---
+++ Structure/Queue.js
@@ -0,0 +1,26 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  push(item) {
+    this.items[this.tail++] = item;
+  }
+
+  pop() {
+    if (this.isEmpty()) return undefined;
+    const item = this.items[this.head];
+    delete this.items[this.head++];
+    return item;
+  }
+
+  isEmpty() {
+    return this.tail === this.head;
+  }
+
+  size() {
+    return this.tail - this.head;
+  }
+}
