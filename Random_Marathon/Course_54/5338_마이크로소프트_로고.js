b826946de16b7e407274b97d3713d5254f902010
zlzonkh1 <zlzonkh1@gmail.com>
2025-06-13 20:50:38 +0900

    boj/5338: 마이크로소프트 로고

---
+++ Random_Marathon/Course_54/5338_마이크로소프트_로고.js
@@ -0,0 +1,8 @@
+const output = [];
+output.push(`       _.-;;-._`);
+output.push(`'-..-'|   ||   |`);
+output.push(`'-..-'|_.-;;-._|`);
+output.push(`'-..-'|   ||   |`);
+output.push(`'-..-'|_.-''-._|`);
+
+console.log(output.join("\n"));
