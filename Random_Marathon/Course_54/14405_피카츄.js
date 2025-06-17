7b10d17c12ff7afcf6dfefab6640cb66b0354792
zlzonkh1 <zlzonkh1@gmail.com>
2025-06-18 00:47:04 +0900

    boj/14405: 피카츄

--- Random_Marathon/Course_54/14405_피카츄.js
+++ Random_Marathon/Course_54/14405_피카츄.js
@@ -9,6 +9,32 @@ const input = require("fs")
   .trim()
   .split("");
 let output = "";
+let checkNext = false;
 const possibleSound = ["pi", "ka", "chu"];
+let stack = [];
+for (let i = 0; i < input.length; i++) {
+  stack.push(input[i]);
+  if (stack.length === 2) {
+    if (stack.join("") === "pi" || stack.join("") === "ka") {
+      stack = [];
+    } else if (stack.join("") === "ch") {
+      checkNext = true;
+    } else {
+      output = "NO";
+      break;
+    }
+  } else if (checkNext) {
+    if (input[i] === "u") {
+      checkNext = false;
+      stack = [];
+    } else {
+      output = "NO";
+      break;
+    }
+  }
+}
+
+if (stack.length > 0) output = "NO";
+else if (output !== "NO" && input.length > 0) output = "YES";
 
 console.log(output);
