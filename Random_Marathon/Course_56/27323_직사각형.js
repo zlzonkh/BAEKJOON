6910f39eb31282aef9f3b457b15ac5c597525cfe
zlzonkh <kinggodkhlive@gmail.com>
2025-06-25 09:06:24 +0900

    boj/27323: 직사각형

---
+++ Random_Marathon/Course_56/27323_직사각형.js
@@ -0,0 +1,13 @@
+const problemNum = 27323;
+const [A, B] = require("fs")
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : "./testcase/" + problemNum + ".txt"
+  )
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+console.log(A * B);
