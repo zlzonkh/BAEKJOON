681561bb2cbb1e2fa60f29a816888a69e1cde8f8
zlzonkh1 <zlzonkh1@gmail.com>
2025-07-20 23:05:04 +0900

    boj/32215: 코드마스터 2024

---
+++ Random_Marathon/Course_59/32215_코드마스터_2024.js
@@ -0,0 +1,13 @@
+const problemNum = 32215;
+const [n, m, k] = require("fs")
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : "./testcase/" + problemNum + ".txt"
+  )
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(m * k + m);
