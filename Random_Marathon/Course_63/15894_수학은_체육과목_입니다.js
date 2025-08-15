52be9680ac0a036b03ff347c1bbe2ea7f62f2ecd
zlzonkh1 <zlzonkh1@gmail.com>
2025-08-15 23:59:04 +0900

    boj/15894: 수학은 체육과목 입니다

---
+++ Random_Marathon/Course_63/15894_수학은_체육과목_입니다.js
@@ -0,0 +1,13 @@
+const problemNum = 15894;
+const input = Number(
+  require("fs")
+    .readFileSync(
+      process.platform === "linux"
+        ? "/dev/stdin"
+        : "./testcase/" + problemNum + ".txt"
+    )
+    .toString()
+    .trim()
+);
+
+console.log(input * 4);
